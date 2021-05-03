3685de303343d5cca2a9a15a48e11c41af1dbe8f
Blake Freeman <blakefreeman@Blakes-MacBook-Air.local>
2021-05-03 01:43:24 -0500

    added win message and game reset

--- InitDraw.py
+++ InitDraw.py
@@ -1,11 +1,12 @@
 import pygame
 import numpy as np
-
-Swidth, SHight = (600,600)
+pygame.font.init() 
+myfont = pygame.font.Font('PressStart2P-Regular.ttf', 20)
+Swidth, SHight = (600,700)
 screen = pygame.display.set_mode((Swidth, SHight))
 
-CellWidth = (Swidth-100)/3
-CellHight = (SHight-100)/3
+CellWidth = (600-100)/3
+CellHight = (600-100)/3
 Margin = 25
 #create Screen
 
@@ -15,22 +16,25 @@ screen.fill((255,255,255))
 pygame.display.set_caption('TicTacStar')
 
 # Create an empty list for storing board
-scale = 3
-grid = np.empty(shape=(scale, scale))
+def DrawStartingBoard(screen):
+	global grid
+	scale = 3
+	grid = np.empty(shape=(scale, scale))
 
-for x in range(scale):
-    for y in range(scale):
-        grid[x,y] = 0 # replace with whatever dynamically assigned value you want there    
+	for x in range(scale):
+	    for y in range(scale):
+	        grid[x,y] = 0 # replace with whatever dynamically assigned value you want there    
 
 
 
-for row in range(3):
-        for column in range(3):
-           
-            pygame.draw.rect(screen,
-                             (0,0,0),
-                             [(Margin + CellWidth) * column + Margin,
-                              (Margin + CellHight) * row + Margin,
-                              CellWidth,
-                              CellHight])
-
+	for row in range(3):
+	        for column in range(3):
+	           
+	            pygame.draw.rect(screen,
+	                             (0,0,0),
+	                             [(Margin + CellWidth) * column + Margin,
+	                              (Margin + CellHight) * row + Margin,
+	                              CellWidth,
+	                              CellHight])
+	
+DrawStartingBoard(screen)
--- main.py
+++ main.py
@@ -1,43 +1,65 @@
 
 import pygame
 import numpy as np
-from InitDraw import screen, grid, CellWidth, CellHight, Margin
+from InitDraw import screen, grid, CellWidth, CellHight, Margin, myfont, DrawStartingBoard
 from MovingOpponet import RandomMovment
 from WinIdint import checkwin
+won = False
 
-running = True
+def mainGame():
+  global grid
+  global won
+  while True:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+      	return
 
-while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-    	running = False
+      elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+ 
+        scale = 3
+        grid = np.empty(shape=(scale, scale))
+        won = False
+        DrawStartingBoard(screen)
 
-    elif event.type == pygame.MOUSEBUTTONDOWN:
-        # User clicks the mouse. Get the position
-        pos = pygame.mouse.get_pos()
-        # Change the x/y screen coordinates to grid coordinates
-        column = int(pos[0] // (CellWidth + Margin))
-        row = int(pos[1] // (CellHight + Margin))
-        
-        # Set that location to one
-        #pygame.draw.rect(screen, (255,0,0),[(((Margin + CellWidth) * column) + Margin),(Margin + CellHight) * row + Margin, CellWidth, CellHight])
-        grid[row][column] = 1
-        RandomMovment(grid)
-        if(checkwin(grid) != False):
-          print(checkwin(grid))
-          running = False
-        if np.count_nonzero(grid)==9:
-        	print("CAT WON")
-   
-  for column in range(len(grid)):
-       for row in range(len(grid[column])):
-           if grid[row,column] == 1:
-                pygame.draw.rect(screen, (0,0,255),[(((Margin + CellWidth) * column) + Margin),(Margin + CellHight) * row + Margin, CellWidth, CellHight])
-           if grid[row,column] == 2:
-                pygame.draw.rect(screen, (255,0,0),[(((Margin + CellWidth) * column) + Margin),(Margin + CellHight) * row + Margin, CellWidth, CellHight])
+      elif event.type == pygame.MOUSEBUTTONDOWN:
+          # User clicks the mouse. Get the position
+          pos = pygame.mouse.get_pos()
+          # Change the x/y screen coordinates to grid coordinates
+          column = int(pos[0] // (CellWidth + Margin))
+          row = int(pos[1] // (CellHight + Margin))
+          if grid[row][column] == 0 and won == False:
+            # Set that location to one
+            #pygame.draw.rect(screen, (255,0,0),[(((Margin + CellWidth) * column) + Margin),(Margin + CellHight) * row + Margin, CellWidth, CellHight])
+            grid[row][column] = 1
+            RandomMovment(grid)
+            if(checkwin(grid) != False):
+              print(checkwin(grid))
+              won = True
+              EndGame(checkwin(grid))
+             
+            elif np.count_nonzero(grid)==9:
+            	EndGame(0)
+     
+    for column in range(len(grid)):
+         for row in range(len(grid[column])):
+             if grid[row,column] == 1:
+                  pygame.draw.rect(screen, (0,0,255),[(((Margin + CellWidth) * column) + Margin),(Margin + CellHight) * row + Margin, CellWidth, CellHight])
+             if grid[row,column] == 2:
+                  pygame.draw.rect(screen, (255,0,0),[(((Margin + CellWidth) * column) + Margin),(Margin + CellHight) * row + Margin, CellWidth, CellHight])
 
-  
-  pygame.display.flip()
+    
+    pygame.display.flip()
 
+def EndGame(winner):
+  pygame.draw.rect(screen,(255,255,255),pygame.Rect(0, 600, 600, 100))
 
+  Players = ["CAT ","You ", "The Computer " ]
+  message = str(Players[int(winner)]+"won the game!")
+  textsurface = myfont.render(message, False, (0, 0, 0))
+  screen.blit(textsurface,(20,600))
+  textsurface = myfont.render("Press 'Space' To Play Again", False, (0, 0, 0))
+  screen.blit(textsurface,(20,650))
 
+ 
+
+mainGame()
